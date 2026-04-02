0646ee128ac254abc4b5f4151ea6b2999ef4a8ce
Marvin Banguero <malejandrobanguero@gmail.com>
2026-04-01 19:53:02 -0500

    setting up backend

--- main.tf
+++ main.tf
@@ -5,16 +5,24 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # ¡LA MAGIA DEL GITOPS AVANZADO!
+  backend "s3" {
+    bucket         = "terraform-backend-kurvas-xyz" # Pon el nombre exacto de tu bucket
+    key            = "laboratorio/jenkins/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
-# Le decimos a Terraform en qué región trabajar (puedes cambiarla si usas otra)
 provider "aws" {
   region = "us-east-1" 
 }
 
-# Nuestro recurso de prueba: Un parámetro de texto gratuito y seguro
 resource "aws_ssm_parameter" "laboratorio_jenkins" {
   name  = "/devops/laboratorio/estado"
   type  = "String"
-  value = "¡Despliegue exitoso desde Jenkins EKS usando seguridad IRSA!"
+  value = "¡Ahora con Backend Remoto en S3 y candado en DynamoDB!"
+  overwrite = true
 }
